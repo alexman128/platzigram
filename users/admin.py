23f22232b102c4a86983534e815f5459cd604970
Alexman <alejandro.mijangos@hotmail.com>
2020-07-19 00:47:59 -0500

    Segunda version de platzigram

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,16 @@
+"""User admin classes"""
+#Django
 from django.contrib import admin
 
+#Models 
+from users.models import Profile
+
 # Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    """Profile Admin"""
+
+    list_display = ('pk', 'user', 'phone_number', 'website', 'picture')
+    list_display_links = ('pk', 'user')
+    list_editable = ('phone_number', 'website')
