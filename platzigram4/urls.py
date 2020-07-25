b2d9be9f626ea8c1306e96f933786b8bf70c50b6
Alexman <alejandro.mijangos@hotmail.com>
2020-07-25 14:59:57 -0500

    Authentication

--- platzigram4/urls.py
+++ platzigram4/urls.py
@@ -22,14 +22,16 @@ from django.conf import settings
 
 from platzigram4 import views as local_views
 from posts import views as posts_views
+from users import views as users_views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello-world/', local_views.hello_world),
-    path('sort/', local_views.sort_integers),
-    path('hi/<str:name>/<int:age>', local_views.say_hi),
-    path('posts/', posts_views.list_posts),
+    path('hello-world/', local_views.hello_world, name='hello-world'),
+    path('sort/', local_views.sort_integers, name='sort'),
+    path('hi/<str:name>/<int:age>', local_views.say_hi, name='hi'),
+    path('posts/', posts_views.list_posts, name='feed'),
+    path('users/login/', users_views.login_view, name='login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
+"""Users views"""
+
+# Django
+from django.contrib.auth import authenticate, login
 from django.shortcuts import render
 
 # Create your views here.
+
+def login_view(request):
+    """Login view"""
+    return render(request, 'users/login.html')
